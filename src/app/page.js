72519ea218d3b5761fb38bb02be90d7c1c01a0e0
icypajamas <ch.hishamabbas@gmail.com>
2025-08-21 17:36:49 +0500

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+"use client";
+import { AuroraBackground } from "./components/ui/aurora-background";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <AuroraBackground className="h-[600px]"/>
+      
+    </div>
+  );
+}
