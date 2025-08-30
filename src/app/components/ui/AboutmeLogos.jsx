09b8299348f6df161008018bfd9a4950b18c1ebe
icypajamas <ch.hishamabbas@gmail.com>
2025-08-30 21:50:28 +0500

    Logo fix + Contact Links

--- src/app/components/ui/AboutmeLogos.jsx
+++ src/app/components/ui/AboutmeLogos.jsx
@@ -1,21 +1,11 @@
 import React from "react";
 import { motion } from "motion/react";
-import { GlowingEffect } from "./glowing-effect";
+
 
 export const AboutmeLogos = () => {
   return (
-    <div className="flex flex-col relative w-240 h-80 border rounded-xl border-white">
-      <div className="flex relative h-20 border rounded-xl border-white">
-        <GlowingEffect
-          blur={0}
-          borderWidth={8}
-          spread={100}
-          glow={true}
-          disabled={false}
-          proximity={64}
-          inactiveZone={0.01}
-        />
-
+    <div className="flex flex-col relative h-80 border rounded-xl border-white">
+      <div className="flex relative h-20 border rounded-xl gap-4 border-white">
         <motion.svg
           viewBox="-52.5 0 361 361"
           xmlns="http://www.w3.org/2000/svg"
@@ -80,7 +70,7 @@ export const AboutmeLogos = () => {
           <path d="M81 79.3 17 0H0v79.3h13.6V17l50.2 62.3H81Zm252.6-.4c-1 0-1.8-.4-2.5-1s-1.1-1.6-1.1-2.6.3-1.8 1-2.5 1.6-1 2.6-1 1.8.3 2.5 1a3.4 3.4 0 0 1 .6 4.3 3.7 3.7 0 0 1-3 1.8zm23.2-33.5h6v23.3c0 2.1-.4 4-1.3 5.5a9.1 9.1 0 0 1-3.8 3.5c-1.6.8-3.5 1.3-5.7 1.3-2 0-3.7-.4-5.3-1s-2.8-1.8-3.7-3.2c-.9-1.3-1.4-3-1.4-5h6c.1.8.3 1.6.7 2.2s1 1.2 1.6 1.5c.7.4 1.5.5 2.4.5 1 0 1.8-.2 2.4-.6a4 4 0 0 0 1.6-1.8c.3-.8.5-1.8.5-3V45.5zm30.9 9.1a4.4 4.4 0 0 0-2-3.3 7.5 7.5 0 0 0-4.3-1.1c-1.3 0-2.4.2-3.3.5-.9.4-1.6 1-2 1.6a3.5 3.5 0 0 0-.3 4c.3.5.7.9 1.3 1.2l1.8 1 2 .5 3.2.8c1.3.3 2.5.7 3.7 1.2a13 13 0 0 1 3.2 1.8 8.1 8.1 0 0 1 3 6.5c0 2-.5 3.7-1.5 5.1a10 10 0 0 1-4.4 3.5c-1.8.8-4.1 1.2-6.8 1.2-2.6 0-4.9-.4-6.8-1.2-2-.8-3.4-2-4.5-3.5a10 10 0 0 1-1.7-5.6h6a5 5 0 0 0 3.5 4.6c1 .4 2.2.6 3.4.6 1.3 0 2.5-.2 3.5-.6 1-.4 1.8-1 2.4-1.7a4 4 0 0 0 .8-2.4c0-.9-.2-1.6-.7-2.2a11 11 0 0 0-2.1-1.4l-3.2-1-3.8-1c-2.8-.7-5-1.7-6.6-3.2a7.2 7.2 0 0 1-2.4-5.7 8 8 0 0 1 1.7-5 10 10 0 0 1 4.3-3.5c2-.8 4-1.2 6.4-1.2 2.3 0 4.4.4 6.2 1.2 1.8.8 3.2 2 4.3 3.4 1 1.4 1.5 3 1.5 5h-5.8z" />
         </motion.svg>
       </div>
-      <div className="flex relative h-20 border rounded-xl border-white">
+      <div className="flex relative h-20 border p-4 rounded-xl border-white">
         <motion.svg
           role="img"
           viewBox="0 0 24 24"
@@ -120,8 +110,8 @@ export const AboutmeLogos = () => {
               y2="58.0107"
               gradientUnits="userSpaceOnUse"
             >
-              <stop stop-color="#387EB8" />
-              <stop offset="1" stop-color="#366994" />
+              <stop stopColor="#387EB8" />
+              <stop offset="1" stopColor="#366994" />
             </linearGradient>
             <linearGradient
               id="paint1_linear_790_3246"
@@ -131,8 +121,8 @@ export const AboutmeLogos = () => {
               y2="90.7623"
               gradientUnits="userSpaceOnUse"
             >
-              <stop stop-color="#FFE052" />
-              <stop offset="1" stop-color="#FFC331" />
+              <stop stopColor="#FFE052" />
+              <stop offset="1" stopColor="#FFC331" />
             </linearGradient>
           </defs>
         </motion.svg>
@@ -166,28 +156,39 @@ export const AboutmeLogos = () => {
           />
         </motion.svg>
       </div>
-      <div className="flex flex-col relative h-20 border rounded-xl top-10 border-white">
-        <h1 className="flex text-white justify-center font-extralight">
-          LINKS
-        </h1>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          viewBox="0 0 128 128"
-          className="w-12 h-12 m-auto"
+      <h1 className="flex text-white justify-center font-extralight pt-4 italic">
+        LINKS
+      </h1>
+      <div className="flex relative border justify-center gap-4 rounded-xl top-5 border-white">
+        <a href="https://github.com/icypajamas" target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            viewBox="0 0 128 128"
+            className="w-12 h-12 cursor-pointer"
+          >
+            <title>Github</title>
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+              fill="#24292f"
+            />
+          </svg>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/hisham-abbas-454210252/"
+          target="_blank"
         >
-          <title>Github</title>
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
-            fill="#24292f"
-          />
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 382 382"
-        className="w-10 h-10 m-auto">
-          <path
-            d="M347.445,0H34.555C15.471,0,0,15.471,0,34.555v312.889C0,366.529,15.471,382,34.555,382h312.889
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 382 382"
+            className="w-10 h-10 cursor-pointer"
+            fill="#0A66C2"
+          >
+            <title>LinkedIn</title>
+            <path
+              d="M347.445,0H34.555C15.471,0,0,15.471,0,34.555v312.889C0,366.529,15.471,382,34.555,382h312.889
 	            C366.529,382,382,366.529,382,347.444V34.555C382,15.471,366.529,0,347.445,0z M118.207,329.844c0,5.554-4.502,10.056-10.056,10.056
 	            H65.345c-5.554,0-10.056-4.502-10.056-10.056V150.403c0-5.554,4.502-10.056,10.056-10.056h42.806
 	            c5.554,0,10.056,4.502,10.056,10.056V329.844z M86.748,123.432c-22.459,0-40.666-18.207-40.666-40.666S64.289,42.1,86.748,42.1
@@ -196,8 +197,9 @@ export const AboutmeLogos = () => {
 	            c0,5.106-4.139,9.246-9.246,9.246h-44.426c-5.106,0-9.246-4.14-9.246-9.246V149.593c0-5.106,4.14-9.246,9.246-9.246h44.426
 	            c5.106,0,9.246,4.14,9.246,9.246v15.655c10.497-15.753,26.097-27.912,59.312-27.912c73.552,0,73.131,68.716,73.131,106.472
 	            L341.91,330.654L341.91,330.654z"
-          />
-        </svg>
+            />
+          </svg>
+        </a>
       </div>
     </div>
   );
